63e29c87796477fbdd5c2d8dfcba8a3136b757eb
davidkngo <davidkngo@outlook.com>
2019-08-09 09:01:33 +0700

    update setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyfingerprint',
+    version='1.5',
+    install_requires=['pyserial', 'pillow'],
+    description='Python written library for using ZhianTec fingerprint sensors.',
+    author='Bastian Raschke',
+    author_email='bastian.raschke@posteo.de',
+    url='https://sicherheitskritisch.de',
+    license='D-FSL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+)
